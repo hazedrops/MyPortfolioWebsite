b01ae5ad6e2ae5a7a510fb16989ee166c5821901
hazedrops <hazedrops@hotmail.com>
2021-04-17 23:17:47 -0700

    Added multiple pages and modified css files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const appProjectDiv = getElementById('appProject');
+const apiProjectDiv = getElementById('apiProject');
+const cloneProjectDiv = getElementById('cloneProject');
+
+appProjectDiv.addEventListener('click', showAppPage);
+apiProjectDiv.addEventListener('click', showApiPage);
+cloneProjectDiv.addEventListener('click', showClonePage);
+
+<div onclick="location.href='http://www.example.com';" style="cursor:pointer;"></div>
+
+function showAppPage() {
+  const location.href='http://www.example.com';
+}
